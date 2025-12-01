44a08fe37055605db24a4339c1f963e4633ade5f
Devin <devinlainediaz@gmail.com>
2025-12-01 11:57:51 -0500

    fix page navigation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-main_page = st.Page("home.py", title="Home", icon="🏠")
-page_2 = st.Page("digenea.py", title="Digenea", icon="✨")
-page_3 = st.Page("cestoda.py", title="Cestoda", icon="✨")
+main_page = st.Page("pages/home.py", title="Home", icon="🏠")
+page_2 = st.Page("pages/digenea.py", title="Digenea", icon="✨")
+page_3 = st.Page("pages/cestoda.py", title="Cestoda", icon="✨")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3])
