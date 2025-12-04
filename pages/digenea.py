7549b605f2274623878a89157c757e5047802979
Devin <devinlainediaz@gmail.com>
2025-12-03 21:20:22 -0500

    add page tabs

--- pages/digenea.py
+++ pages/digenea.py
@@ -4,70 +4,73 @@ from utils import parasite_card
 
 st.title("Digenea")
 
-row1 = st.columns(3)
-row2 = st.columns(3)
-row3 = st.columns(3)
+tab1, tab2, tab3 = st.tabs(["Overview", "Species Examples", "tbd"])
 
 
-with row1[0]:
-    parasite_card(
-        "Schistosoma mansoni",
-        "",
-        "Causes intestinal schistosomiasis in humans",
-    )
+with tab2:
+    row1 = st.columns(3)
+    row2 = st.columns(3)
+    row3 = st.columns(3)
 
-with row1[1]:
-    parasite_card(
-        "Schistosoma haematobium",
-        "",
-        "Causes urinary schistosomiasis in humans"
-    )
+    with row1[0]:
+        parasite_card(
+            "Schistosoma mansoni",
+            "",
+            "Causes intestinal schistosomiasis in humans",
+        )
 
-with row1[2]:
-    parasite_card(
-        "Schistosoma japonicum",
-        "",
-        "Causes intestinal schistosomiasis in humans"
-    )
+    with row1[1]:
+        parasite_card(
+            "Schistosoma haematobium",
+            "",
+            "Causes urinary schistosomiasis in humans"
+        )
 
-with row2[0]:
-    parasite_card(
-        "Fasciola hepatica",
-        "",
-        "Causes fascioliasis in livestock and humans"
-    )
+    with row1[2]:
+        parasite_card(
+            "Schistosoma japonicum",
+            "",
+            "Causes intestinal schistosomiasis in humans"
+        )
 
-with row2[1]:
-    parasite_card(
-        "Clonorchis sinensis",
-        "",
-        "Causes clonorchiasis in humans"
-    )
+    with row2[0]:
+        parasite_card(
+            "Fasciola hepatica",
+            "",
+            "Causes fascioliasis in livestock and humans"
+        )
 
-with row2[2]:
-    parasite_card(
-        "Paragonimus westermani",
-        "",
-        "Causes paragonimiasis in humans"
-    )
+    with row2[1]:
+        parasite_card(
+            "Clonorchis sinensis",
+            "",
+            "Causes clonorchiasis in humans"
+        )
 
-with row3[0]:
-    parasite_card(
-        "Dicrocoelium dendriticum",
-        "",
-        "Causes dicrocoeliasis in livestock"
-    )
+    with row2[2]:
+        parasite_card(
+            "Paragonimus westermani",
+            "",
+            "Causes paragonimiasis in humans"
+        )
 
-with row3[1]:
-    parasite_card(
-        "Ribeiroia ondatrae",
-        "",
-        "Causes limb malformations in amphibians"
-    )
+    with row3[0]:
+        parasite_card(
+            "Dicrocoelium dendriticum",
+            "",
+            "Causes dicrocoeliasis in livestock"
+        )
 
-with row3[2]:
-    parasite_card(
-        "Leucochloridium variae",
-        "",
-        "Infects birds & snails, causing parasitic castration"
-    )
+    with row3[1]:
+        parasite_card(
+            "Ribeiroia ondatrae",
+            "",
+            "Causes limb malformations in amphibians"
+        )
+
+    with row3[2]:
+        parasite_card(
+            "Leucochloridium variae",
+            "",
+            "Infects birds & snails, causing parasitic castration"
+        )
